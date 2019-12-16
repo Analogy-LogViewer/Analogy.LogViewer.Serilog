55f535241c95771ade0af5a7d14ffe36f0e711e0
Lior Banai <liorbanai@gmail.com>
2019-12-16 20:43:19 +0200

    wip

---
+++ Analogy.LogViewer.Serilog/Analogy.LogViewer.Serilog/AnalogySink.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+using Serilog.Formatting.Json;
+
+namespace Analogy.LogViewer.Serilog
+{
+    class AnalogySink : ILogEventSink
+    {
+        readonly ITextFormatter _textFormatter;
+
+        public AnalogySink(ITextFormatter textFormatter)
+        {
+            if (textFormatter == null) throw new ArgumentNullException(nameof(textFormatter));
+            _textFormatter = textFormatter;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            var sr = new StringWriter();
+            _textFormatter.Format(logEvent, sr);
+
+            var text = sr.ToString().Trim();
+
+            if (logEvent.Level == LogEventLevel.Error || logEvent.Level == LogEventLevel.Fatal)
+                Trace.TraceError(text);
+            else if (logEvent.Level == LogEventLevel.Warning)
+                Trace.TraceWarning(text);
+            else if (logEvent.Level == LogEventLevel.Information)
+                Trace.TraceInformation(text);
+            else
+                Trace.WriteLine(text);
+        }
+    }
+
+    public static class TraceLoggerConfigurationExtensions
+    {
+        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+
+        /// <summary>
+        /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for
+        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+        /// to be changed at runtime.</param>
+        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
+        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration Analogy(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            string outputTemplate = DefaultOutputTemplate,
+            IFormatProvider formatProvider = null,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            return Analogy(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
+        }
+
+        /// <summary>
+        /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for
+        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+        /// to be changed at runtime.</param>
+        /// <param name="formatter">A custom formatter to apply to the output events. This can be used with
+        /// e.g. <see cref="JsonFormatter"/> to produce JSON output. To customize the text layout only, use the
+        /// overload that accepts an output template instead.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration Analogy(
+            this LoggerSinkConfiguration sinkConfiguration,
+            ITextFormatter formatter,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+            return sinkConfiguration.Sink(new AnalogySink(formatter), restrictedToMinimumLevel, levelSwitch);
+        }
+    }
+}
--- Analogy.LogViewer.Serilog/Analogy.LogViewer.Serilog/ClefParser.cs
+++ Analogy.LogViewer.Serilog/Analogy.LogViewer.Serilog/ClefParser.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Analogy.Interfaces;
+using Newtonsoft.Json;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact.Reader;
@@ -18,35 +19,43 @@ namespace Analogy.LogViewer.Serilog
         public async Task<IEnumerable<AnalogyLogMessage>> Process(string fileName, CancellationToken token, ILogMessageCreatedHandler messagesHandler)
         {
             var messages = await Task<IEnumerable<AnalogyLogMessage>>.Factory.StartNew(() =>
-         {
-             List<AnalogyLogMessage> parsedMessages = new List<AnalogyLogMessage>();
-             try
-             {
-                 using (var clef = File.OpenText(fileName))
-                 {
-                     var reader = new LogEventReader(clef);
-                     LogEvent evt;
-                     while (reader.TryRead(out evt))
-                     {
-                         var m = ParseEvent(evt);
-                         parsedMessages.Add(m);
-                     }
-                 }
-                 messagesHandler.AppendMessages(parsedMessages, fileName);
-                 return parsedMessages;
-             }
-             catch (Exception e)
-             {
-                 AnalogyLogMessage empty = new AnalogyLogMessage($"Error reading file {fileName}: Error: {e.Message}", AnalogyLogLevel.Error, AnalogyLogClass.General, "Analogy", "None");
-                 empty.Source = nameof(ClefParser);
-                 empty.Module = "Analogy.LogViewer.Serilog";
-                 parsedMessages.Add(empty);
-                 messagesHandler.AppendMessages(parsedMessages, fileName);
-                 return parsedMessages;
-             }
+            {
+                List<AnalogyLogMessage> parsedMessages = new List<AnalogyLogMessage>();
+                try
+                {
+                    using (var analogy = new LoggerConfiguration()
+                        .WriteTo.Analogy()
+                        .CreateLogger())
+                    {
+                        using (var clef = File.OpenText(fileName))
+                        {
+                            var reader = new LogEventReader(clef);
+                            LogEvent evt;
+                            while (reader.TryRead(out evt))
+                            {
+                                analogy.Write(evt);
+                                var m = ParseEvent(evt);
+                                parsedMessages.Add(m);
+                            }
+                        }
+
+                        messagesHandler.AppendMessages(parsedMessages, fileName);
+                        return parsedMessages;
+                    }
+                }
+                catch (Exception e)
+                {
+                    AnalogyLogMessage empty = new AnalogyLogMessage($"Error reading file {fileName}: Error: {e.Message}",
+                        AnalogyLogLevel.Error, AnalogyLogClass.General, "Analogy", "None");
+                    empty.Source = nameof(ClefParser);
+                    empty.Module = "Analogy.LogViewer.Serilog";
+                    parsedMessages.Add(empty);
+                    messagesHandler.AppendMessages(parsedMessages, fileName);
+                    return parsedMessages;
+                }
 
 
-         });
+            });
             return messages;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
