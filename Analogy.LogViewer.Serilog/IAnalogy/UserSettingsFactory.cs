8f36bb2ee61a1dd3f1b4ea2ced24f4f085433415
Lior Banai <liorbanai@gmail.com>
2020-03-14 21:38:52 +0200

    related to Analogy-LogViewer/Analogy.LogViewer#113

--- Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
+++ Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Analogy.DataProviders.Extensions;
+using Analogy.LogViewer.Serilog.Properties;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Analogy.LogViewer.Serilog.Managers;
-using Analogy.LogViewer.Serilog.Properties;
 
 namespace Analogy.LogViewer.Serilog.IAnalogy
 {
-   public class UserSettingsFactory:Analogy.DataProviders.Extensions.IAnalogyDataProviderSettings
-   {
-       public string Title { get; } = "Serilog User Settings";
-        public UserControl DataProviderSettings { get; }=new SerilogUCSettings();
-        public Image Icon { get; } = Resources.AnalogySerilog16x16;
+    public class UserSettingsFactory : IAnalogyDataProviderSettings
+    {
+        public string Title { get; } = "Serilog User Settings";
+        public UserControl DataProviderSettings { get; } = new SerilogUCSettings();
+        public Image SmallImage { get; } = Resources.AnalogySerilog16x16;
+        public Image LargeImage { get; } = Resources.AnalogySerilog32x32;
+        public Guid FactoryId { get; set; } = new Guid("513A4393-425E-4054-92D4-6A816983E51F");
 
         public Task SaveSettingsAsync()
         {
-            ((SerilogUCSettings) DataProviderSettings)?.SaveSettings();
+            ((SerilogUCSettings)DataProviderSettings)?.SaveSettings();
             return Task.CompletedTask;
         }
-        }
+    }
 }
