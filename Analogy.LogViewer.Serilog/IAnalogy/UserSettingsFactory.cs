1cc2300b85eb27d79a0a321aa2eb2c6d284ea6d5
Lior Banai <liorbanai@gmail.com>
2020-04-10 13:02:37 +0300

    update interface

--- Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
+++ Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
@@ -14,6 +14,7 @@ namespace Analogy.LogViewer.Serilog.IAnalogy
         public Image SmallImage { get; } = Resources.AnalogySerilog16x16;
         public Image LargeImage { get; } = Resources.AnalogySerilog32x32;
         public Guid FactoryId { get; set; } = new Guid("513A4393-425E-4054-92D4-6A816983E51F");
+        public Guid ID { get; set; } = new Guid("26FF0D4D-8FA8-46C6-A021-079E669E7EC6");
 
         public Task SaveSettingsAsync()
         {
