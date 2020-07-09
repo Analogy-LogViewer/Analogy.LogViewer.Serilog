8ababf44827e84c4d6b9e50dcd25375eee5c76c5
Lior Banai <liorbanai@gmail.com>
2020-07-09 09:13:31 +0300

    add header image option

---
+++ Analogy.LogViewer.Serilog/IAnalogy/SerilogAnalogyComponentImages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.DataProviders.Extensions;
+using Analogy.LogViewer.Serilog.Properties;
+
+namespace Analogy.LogViewer.Serilog.IAnalogy
+{
+    public class SerilogAnalogyComponentImages : IAnalogyComponentImages
+    {
+        public Image GetLargeImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == PrimaryFactory.Id)
+                return Resources.AnalogySerilog32x32;
+            return null;
+        }
+
+        public Image GetSmallImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == PrimaryFactory.Id)
+                return Resources.AnalogySerilog16x16;
+            return null;
+        }
+
+        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    }
+}
