6e1c754678d2569d90b0a79c03493ef05dc477d3
Lior Banai <liorbanai@gmail.com>
2020-07-22 20:26:23 +0300

    Support for reading compressed files. Fix #45

--- Analogy.LogViewer.Serilog/SerilogSettings.cs
+++ Analogy.LogViewer.Serilog/SerilogSettings.cs
@@ -24,8 +24,8 @@ namespace Analogy.LogViewer.Serilog
             Format = SerilogFileFormat.CLEF;
             Directory = string.Empty;
             IgnoredAttributes = new List<string>() { "N" };
-            FileOpenDialogFilters = "All Supported formats (*.Clef;*.log)|*.clef;*.log|Clef format (*.clef)|*.clef|Plain log text file (*.log)|*.log";
-            SupportFormats = new List<string> { "*.Clef", "*.log" };
+            FileOpenDialogFilters = "All Supported formats (*.Clef;*.log;*.gz)|*.clef;*.log;*.gz|Clef format (*.clef)|*.clef|Plain log text file (*.log)|*.log|GZIP file (*.gz)|*.gz";
+            SupportFormats = new List<string> { "*.Clef", "*.log", "*.gz" };
             RegexPatterns = new List<RegexPattern>();
             RegexPatterns.Add(new RegexPattern(@"\$(?<Date>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3})+\|+(?<Thread>\d+)+\|(?<Level>\w+)+\|+(?<Source>.*)\|(?<Text>.*)", "yyyy-MM-dd HH:mm:ss,fff", ""));
 
