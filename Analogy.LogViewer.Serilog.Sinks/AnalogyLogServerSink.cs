9b5f61c9dd93e059370d99ec5360363c5fd74adc
Lior Banai <liorbanai@gmail.com>
2020-08-24 21:26:54 +0300

    ..

--- Analogy.LogViewer.Serilog.Sinks/AnalogyLogServerSink.cs
+++ Analogy.LogViewer.Serilog.Sinks/AnalogyLogServerSink.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Analogy.Interfaces;
 using Analogy.LogServer.Clients;
-using Serilog;
 using Serilog.Core;
 using Serilog.Events;
-using Serilog.Configuration;
+using System;
 
 namespace Analogy.LogViewer.Serilog.Sinks
 {
@@ -20,11 +19,137 @@ namespace Analogy.LogViewer.Serilog.Sinks
         public void Emit(LogEvent logEvent)
         {
 
-            var message = logEvent.RenderMessage(_formatProvider);
-            var alm = CommonParser.ParseLogEventProperties(logEvent);
-            alm.Text = message;
+            var alm = ParseLogEventProperties(logEvent, in _formatProvider);
             logServerMessageProducer?.Log(alm.Text, alm.Source, alm.Level, alm.Category, alm.MethodName, alm.LineNumber,
                 alm.FileName);
+        }
+
+        public static AnalogyLogMessage ParseLogEventProperties(LogEvent evt, in IFormatProvider formatProvider)
+        {
+            AnalogyLogMessage m = new AnalogyLogMessage();
+
+            switch (evt.Level)
+            {
+                case LogEventLevel.Verbose:
+                    m.Level = AnalogyLogLevel.Verbose;
+                    break;
+                case LogEventLevel.Debug:
+                    m.Level = AnalogyLogLevel.Debug;
+                    break;
+                case LogEventLevel.Information:
+                    m.Level = AnalogyLogLevel.Event;
+                    break;
+                case LogEventLevel.Warning:
+                    m.Level = AnalogyLogLevel.Warning;
+                    break;
+                case LogEventLevel.Error:
+                    m.Level = AnalogyLogLevel.Error;
+                    break;
+                case LogEventLevel.Fatal:
+                    m.Level = AnalogyLogLevel.Critical;
+                    break;
+                default:
+                    {
+                        m.Level = AnalogyLogLevel.Unknown;
+                        break;
+                    }
+            }
+
+            m.Date = evt.Timestamp.DateTime;
+            m.Text = evt.RenderMessage(formatProvider);
+            if (evt.Properties.TryGetValue(Constants.ProcessName, out var processName))
+            {
+                if (processName is ScalarValue scalarValue &&
+                    scalarValue.Value is string processNameString)
+                {
+                    m.Module = processNameString;
+                }
+            }
+
+            if (evt.Properties.TryGetValue(Constants.Source, out var sourceContext))
+            {
+                if (sourceContext is ScalarValue scalarValue && scalarValue.Value is string sourceContextString)
+                {
+                    m.Source = sourceContextString;
+                }
+                else
+                {
+                    m.Source = sourceContext.ToString();
+                }
+            }
+
+            if (evt.Properties.TryGetValue(Constants.ThreadId, out var threadId))
+            {
+                if (threadId is ScalarValue scalarValue)
+                {
+                    if (scalarValue.Value is int intValue)
+                    {
+                        m.ThreadId = intValue;
+                    }
+                    else if (scalarValue.Value is long longValue && longValue <= int.MaxValue)
+                    {
+                        m.ThreadId = (int)longValue;
+                    }
+                }
+            }
+            if (evt.Properties.TryGetValue(Constants.ProcessId, out var processId))
+            {
+                if (processId is ScalarValue scalarValue)
+                {
+                    if (scalarValue.Value is int intValue)
+                    {
+                        m.ProcessId = intValue;
+                    }
+                    else if (scalarValue.Value is long longValue && longValue <= int.MaxValue)
+                    {
+                        m.ProcessId = (int)longValue;
+                    }
+                }
+            }
+            if (evt.Properties.TryGetValue(Constants.MachineName, out var machineName))
+            {
+                if (machineName is ScalarValue scalarValue &&
+                    scalarValue.Value is string machineNameString)
+                {
+                    m.MachineName = machineNameString;
+                }
+            }
+            if (evt.Properties.TryGetValue(Constants.EnvironmentUserName, out var environmentUserName))
+            {
+                if (environmentUserName is ScalarValue scalarValue &&
+                    scalarValue.Value is string environmentUserNameString)
+                {
+                    m.User = environmentUserNameString;
+                }
+            }
+            if (evt.Properties.TryGetValue(Constants.User, out var environmentUser))
+            {
+                if (environmentUser is ScalarValue scalarValue &&
+                    scalarValue.Value is string environmentUserString)
+                {
+                    m.User = environmentUserString;
+                }
+                if (environmentUser is StructureValue structure)
+
+                {
+                    m.User = structure.ToString();
+                }
+            }
+            //m.AdditionalInformation = new Dictionary<string, string>();
+            //foreach (KeyValuePair<string, LogEventPropertyValue> property in evt.Properties)
+            //{
+            //    if (property.Key.Equals(Constants.EnvironmentUserName) ||
+            //        property.Key.Equals(Constants.Source) ||
+            //        property.Key.Equals(Constants.ThreadId) ||
+            //        property.Key.Equals(Constants.ProcessName) ||
+            //        property.Key.Equals(Constants.MachineName) ||
+            //        property.Key.Equals(Constants.User) ||
+            //        property.Key.Equals(Constants.ProcessId) ||
+            //        UserSettingsManager.UserSettings.Settings.IgnoredAttributes.Contains(property.Key))
+            //        continue;
+            //    m.AdditionalInformation.Add(property.Key, property.Value.ToString());
+            //}
+            return m;
         }
 
         public void Dispose()
---
+++ Analogy.LogViewer.Serilog.Sinks/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Analogy.LogViewer.Serilog.Sinks
+{
+    public static class Constants
+    {
+        public static string Source { get; } = global::Serilog.Core.Constants.SourceContextPropertyName;
+        public static string ThreadId { get; } = "ThreadId";
+
+        public static string ProcessId { get; } = "ProcessId";
+        public static string ProcessName { get; } = "ProcessName";
+        public static string MachineName { get; } = "MachineName";
+        public static string User { get; } = "User";
+        public static string EnvironmentUserName { get; } = "EnvironmentUserName";
+
+        public static Dictionary<string, string> GetFieldValues()
+        {
+            return typeof(Constants)
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(f => f.FieldType == typeof(string))
+                .ToDictionary(f => f.Name, f => (string)f.GetValue(null));
+        }
+
+    }
+}
