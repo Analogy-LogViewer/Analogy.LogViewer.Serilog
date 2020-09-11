ab6dbcef67567eee649110996e44594716c76188
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-09-11 10:07:52 +0300

    update factory id

--- Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
+++ Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
@@ -13,7 +13,7 @@ namespace Analogy.LogViewer.Serilog.IAnalogy
         public UserControl DataProviderSettings { get; } = new SerilogUCSettings();
         public Image SmallImage { get; set; } = Resources.AnalogySerilog16x16;
         public Image LargeImage { get; set; } = Resources.AnalogySerilog32x32;
-        public Guid FactoryId { get; set; } = new Guid("513A4393-425E-4054-92D4-6A816983E51F");
+        public Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public Guid Id { get; set; } = new Guid("26FF0D4D-8FA8-46C6-A021-079E669E7EC6");
 
         public Task SaveSettingsAsync()
