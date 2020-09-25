a9b2221e35afa6a87260087ba7a334e18a22cb94
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-09-26 00:22:14 +0300

    update tests

--- Analogy.LogViewer.Serilog.UnitTests/JsonFormatTests.cs
+++ Analogy.LogViewer.Serilog.UnitTests/JsonFormatTests.cs
@@ -38,7 +38,7 @@ namespace Analogy.LogViewer.Serilog.UnitTests
             MessageHandlerForTesting forTesting = new MessageHandlerForTesting();
             var messages = (await p.Process(fileName, cts.Token, forTesting)).ToList();
             Assert.IsTrue(messages.Count == 2);
-            Assert.IsTrue(messages[0].Text == "Hello, { Name: \"nblumhardt\", Tags: [1, 2, 3] }, 0000007b at 06/07/2016 06:44:57");
+            Assert.IsTrue(messages[0].Text == "Hello, { Name: \"nblumhardt\", Tags: [1, 2, 3] }, 0000007b at 06/07/2016 06:44:57","got"+ messages[0].Text);
             Assert.IsTrue(messages[0].User == "{ Name: \"nblumhardt\", Tags: [1, 2, 3] }");
         }
 
