f8e014393c09e4cdca3a01fd652226105c2febd4
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-30 09:22:15 +0200

    remve references to regex package

--- Analogy.LogViewer.Serilog/SerilogSettings.cs
+++ Analogy.LogViewer.Serilog/SerilogSettings.cs
@@ -1,5 +1,4 @@
-﻿using Analogy.LogViewer.RegexParser;
-using Analogy.LogViewer.Serilog.DataTypes;
+﻿using Analogy.LogViewer.Serilog.DataTypes;
 using System.Collections.Generic;
 
 namespace Analogy.LogViewer.Serilog
@@ -10,7 +9,6 @@ namespace Analogy.LogViewer.Serilog
         public string FileOpenDialogFilters { get; set; }
         public string FileSaveDialogFilters { get; } = string.Empty;
         public List<string> SupportFormats { get; set; }
-        public List<RegexPattern> RegexPatterns { get; set; }
         public string Directory { get; set; }
         public FileFormat Format { get; set; }
         public FileFormatDetection FileFormatDetection { get; set; }
@@ -24,9 +22,6 @@ namespace Analogy.LogViewer.Serilog
             IgnoredAttributes = new List<string>() { "N" };
             FileOpenDialogFilters = "All Supported formats (*.Clef;*.log;*.gz)|*.clef;*.log;*.gz|Clef format (*.clef)|*.clef|Plain log text file (*.log)|*.log|GZIP file (*.gz)|*.gz";
             SupportFormats = new List<string> { "*.Clef", "*.log", "*.gz" };
-            RegexPatterns = new List<RegexPattern>();
-            //  RegexPatterns.Add(new RegexPattern(@"\$(?<Date>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3})+\|+(?<Thread>\d+)+\|(?<Level>\w+)+\|+(?<Source>.*)\|(?<Text>.*)", "yyyy-MM-dd HH:mm:ss,fff", ""));
-
         }
 
 
