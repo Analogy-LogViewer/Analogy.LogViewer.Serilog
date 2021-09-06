b912df5b0fc2bdd373c5ca5046647d73e7f67afe
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-09-06 18:48:48 +0300

    addaspnet expamle project (WIP) #314

---
+++ Analogy.LogViewer.Serilog.AspNet/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Analogy.LogViewer.Serilog.Sinks;
+using Serilog;
+
+namespace Analogy.LogViewer.Serilog.AspNet
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
