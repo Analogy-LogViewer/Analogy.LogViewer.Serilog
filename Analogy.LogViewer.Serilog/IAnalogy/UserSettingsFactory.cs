ebfdb25b9d3ecf28333bdafa8f56bb5345e3a955
Lior Banai <liorbanai@gmail.com>
2022-10-27 11:37:33 +0300

    update nugets and align version number with lower level major version

--- Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
+++ Analogy.LogViewer.Serilog/IAnalogy/UserSettingsFactory.cs
@@ -3,17 +3,23 @@ using System;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Analogy.Interfaces;
 
 namespace Analogy.LogViewer.Serilog.IAnalogy
 {
-    public class UserSettingsFactory : Analogy.LogViewer.Template.UserSettingsFactory
+    public class UserSettingsFactory : Analogy.LogViewer.Template.TemplateUserSettingsFactory
     {
         public override string Title { get; set; } = "Serilog User Settings";
-        public override UserControl DataProviderSettings { get; set; } = new SerilogUCSettings();
+        public override UserControl DataProviderSettings { get; set; }
         public override Image SmallImage { get; set; } = Resources.AnalogySerilog16x16;
         public override Image LargeImage { get; set; } = Resources.AnalogySerilog32x32;
         public override Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public override Guid Id { get; set; } = new Guid("26FF0D4D-8FA8-46C6-A021-079E669E7EC6");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+            DataProviderSettings = new SerilogUCSettings();
+        }
 
         public override Task SaveSettingsAsync()
         {
