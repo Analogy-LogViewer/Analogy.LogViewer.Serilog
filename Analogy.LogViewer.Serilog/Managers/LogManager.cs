da1ba4c22d2efd8c06f1d3421dcd4ea202a3159d
Lior Banai <liorbanai@gmail.com>
2022-11-09 14:46:33 +0200

    NET7

--- Analogy.LogViewer.Serilog/Managers/LogManager.cs
+++ Analogy.LogViewer.Serilog/Managers/LogManager.cs
@@ -1,5 +1,6 @@
 ﻿using Analogy.Interfaces;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace Analogy.LogViewer.Serilog.Managers
 {
@@ -7,28 +8,44 @@ namespace Analogy.LogViewer.Serilog.Managers
     {
         private static Lazy<LogManager> _instance = new Lazy<LogManager>();
         public static LogManager Instance => _instance.Value;
-        private IAnalogyLogger Logger { get; set; }
+        private IAnalogyLogger? Logger { get; set; }
 
         public void SetLogger(IAnalogyLogger logger) => Logger = logger;
         public void LogInformation(string message, string source = "", string memberName = "", int lineNumber = 0, string filePath = "")
-            => Logger.LogInformation(message, source, memberName, lineNumber, filePath);
+            => Logger?.LogInformation(message, source, memberName, lineNumber, filePath);
 
         public void LogWarning(string message, string source = "", string memberName = "", int lineNumber = 0, string filePath = "")
-            => Logger.LogWarning(message, source, memberName, lineNumber, filePath);
+            => Logger?.LogWarning(message, source, memberName, lineNumber, filePath);
 
         public void LogDebug(string message, string source = "", string memberName = "", int lineNumber = 0, string filePath = "")
-            => Logger.LogDebug(message, source, memberName, lineNumber, filePath);
+            => Logger?.LogDebug(message, source, memberName, lineNumber, filePath);
 
         public void LogError(string message, string source = "", string memberName = "", int lineNumber = 0, string filePath = "")
-            => Logger.LogError(message, source, memberName, lineNumber, filePath);
+            => Logger?.LogError(message, source, memberName, lineNumber, filePath);
 
         public void LogCritical(string message, string source = "", string memberName = "", int lineNumber = 0, string filePath = "")
-            => Logger.LogCritical(message, source, memberName, lineNumber, filePath);
+            => Logger?.LogCritical(message, source, memberName, lineNumber, filePath);
         public void LogException(string message, Exception ex, string source, string memberName = "",
             int lineNumber = 0,
             string filePath = "")
         {
-            Logger.LogException(message,ex, source,  memberName, lineNumber, filePath);
+            Logger?.LogException(message, ex, source, memberName, lineNumber, filePath);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            Logger?.Log(logLevel, eventId, state, exception, formatter);
+
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return Logger?.IsEnabled(logLevel) ?? false;
+        }
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return Logger?.BeginScope(state);
         }
     }
 }
