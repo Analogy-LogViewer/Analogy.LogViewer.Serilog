1e0c817b48c300e895100f73a68a9f6fe7560f26
Lior Banai <liorbanai@gmail.com>
2023-02-11 15:51:27 +0200

    update nugets

--- Analogy.LogViewer.Serilog.Sinks/AnalogyLogServerSink.cs
+++ Analogy.LogViewer.Serilog.Sinks/AnalogyLogServerSink.cs
@@ -3,6 +3,7 @@ using Analogy.LogServer.Clients;
 using Serilog.Core;
 using Serilog.Events;
 using System;
+using System.Collections.Generic;
 
 namespace Analogy.LogViewer.Serilog.Sinks
 {
@@ -78,14 +79,9 @@ namespace Analogy.LogViewer.Serilog.Sinks
 
             if (evt.Properties.TryGetValue(Constants.Source, out var sourceContext))
             {
-                if (sourceContext is ScalarValue scalarValue && scalarValue.Value is string sourceContextString)
-                {
-                    m.Source = sourceContextString;
-                }
-                else
-                {
-                    m.Source = sourceContext.ToString();
-                }
+                m.Source = sourceContext is ScalarValue { Value: string sourceContextString }
+                    ? sourceContextString
+                    : sourceContext.ToString();
             }
 
             if (evt.Properties.TryGetValue(Constants.ThreadId, out var threadId))
@@ -96,7 +92,7 @@ namespace Analogy.LogViewer.Serilog.Sinks
                     {
                         m.ThreadId = intValue;
                     }
-                    else if (scalarValue.Value is long longValue && longValue <= int.MaxValue)
+                    else if (scalarValue.Value is long longValue and <= int.MaxValue)
                     {
                         m.ThreadId = (int)longValue;
                     }
@@ -110,7 +106,7 @@ namespace Analogy.LogViewer.Serilog.Sinks
                     {
                         m.ProcessId = intValue;
                     }
-                    else if (scalarValue.Value is long longValue && longValue <= int.MaxValue)
+                    else if (scalarValue.Value is long longValue and <= int.MaxValue)
                     {
                         m.ProcessId = (int)longValue;
                     }
@@ -118,24 +114,21 @@ namespace Analogy.LogViewer.Serilog.Sinks
             }
             if (evt.Properties.TryGetValue(Constants.MachineName, out var machineName))
             {
-                if (machineName is ScalarValue scalarValue &&
-                    scalarValue.Value is string machineNameString)
+                if (machineName is ScalarValue { Value: string machineNameString })
                 {
                     m.MachineName = machineNameString;
                 }
             }
             if (evt.Properties.TryGetValue(Constants.EnvironmentUserName, out var environmentUserName))
             {
-                if (environmentUserName is ScalarValue scalarValue &&
-                    scalarValue.Value is string environmentUserNameString)
+                if (environmentUserName is ScalarValue { Value: string environmentUserNameString })
                 {
                     m.User = environmentUserNameString;
                 }
             }
             if (evt.Properties.TryGetValue(Constants.User, out var environmentUser))
             {
-                if (environmentUser is ScalarValue scalarValue &&
-                    scalarValue.Value is string environmentUserString)
+                if (environmentUser is ScalarValue { Value: string environmentUserString })
                 {
                     m.User = environmentUserString;
                 }
@@ -145,27 +138,28 @@ namespace Analogy.LogViewer.Serilog.Sinks
                     m.User = structure.ToString();
                 }
             }
-            //m.AdditionalInformation = new Dictionary<string, string>();
-            //foreach (KeyValuePair<string, LogEventPropertyValue> property in evt.Properties)
-            //{
-            //    if (property.Key.Equals(Constants.EnvironmentUserName) ||
-            //        property.Key.Equals(Constants.Source) ||
-            //        property.Key.Equals(Constants.ThreadId) ||
-            //        property.Key.Equals(Constants.ProcessName) ||
-            //        property.Key.Equals(Constants.MachineName) ||
-            //        property.Key.Equals(Constants.User) ||
-            //        property.Key.Equals(Constants.ProcessId) ||
-            //        UserSettingsManager.UserSettings.Settings.IgnoredAttributes.Contains(property.Key))
-            //        continue;
-            //    m.AdditionalInformation.Add(property.Key, property.Value.ToString());
-            //}
+            m.AdditionalInformation = new Dictionary<string, string>();
+           foreach (KeyValuePair<string, LogEventPropertyValue> property in evt.Properties)
+           {
+               if (property.Key.Equals(Constants.EnvironmentUserName) ||
+                   property.Key.Equals(Constants.Source) ||
+                   property.Key.Equals(Constants.ThreadId) ||
+                   property.Key.Equals(Constants.ProcessName) ||
+                   property.Key.Equals(Constants.MachineName) ||
+                   property.Key.Equals(Constants.User) ||
+                   property.Key.Equals(Constants.ProcessId))
+               {
+                   continue;
+               }
+               m.AdditionalInformation.Add(property.Key, property.Value.ToString());
+           }
             return m;
         }
 
         public void Dispose()
         {
-            logServerMessageProducer = null;
-            //logServerMessageProducer.StopReceiving();
+            logServerMessageProducer.StopReceiving();
+            logServerMessageProducer.Dispose();
         }
     }
 }
