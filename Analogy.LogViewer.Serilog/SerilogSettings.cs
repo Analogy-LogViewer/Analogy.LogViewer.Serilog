727cab81b4ad575d396c1e1bee510f0a2ad448ea
Lior Banai <liorbanai@gmail.com>
2023-09-02 15:23:45 +0300

    update nugets

--- Analogy.LogViewer.Serilog/SerilogSettings.cs
+++ Analogy.LogViewer.Serilog/SerilogSettings.cs
@@ -7,26 +7,12 @@ namespace Analogy.LogViewer.Serilog
     public class SerilogSettings
     {
         public bool UseApplicationFolderForSettings { get; set; }
-        public string FileOpenDialogFilters { get; set; }
+        public string FileOpenDialogFilters { get; set; } = "All Supported formats (*.Clef;*.log;*.gz)|*.clef;*.log;*.gz;*.zip|Clef format (*.clef)|*.clef|Plain log text file (*.log)|*.log|GZIP file (*.gz)|*.gz|ZIP file (*.zip)|*.zip";
         public string FileSaveDialogFilters { get; } = string.Empty;
-        public List<string> SupportFormats { get; set; }
-        public string Directory { get; set; }
-        public FileFormat Format { get; set; }
-        public FileFormatDetection FileFormatDetection { get; set; }
-        public List<string> IgnoredAttributes { get; set; }
-
-        public SerilogSettings()
-        {
-            FileFormatDetection = FileFormatDetection.Automatic;
-            Format = FileFormat.Unknown;
-            Directory = string.Empty;
-            IgnoredAttributes = new List<string>() { "N" };
-            FileOpenDialogFilters = "All Supported formats (*.Clef;*.log;*.gz)|*.clef;*.log;*.gz;*.zip|Clef format (*.clef)|*.clef|Plain log text file (*.log)|*.log|GZIP file (*.gz)|*.gz|ZIP file (*.zip)|*.zip";
-            SupportFormats = new List<string> { "*.Clef", "*.log"};
-        }
-
-
-
-
+        public List<string> SupportFormats { get; set; } = new() { "*.Clef", "*.log"};
+        public string Directory { get; set; } = string.Empty;
+        public FileFormat Format { get; set; } = FileFormat.Unknown;
+        public FileFormatDetection FileFormatDetection { get; set; } = FileFormatDetection.Automatic;
+        public List<string> IgnoredAttributes { get; set; } = new() { "N" };
     }
 }
