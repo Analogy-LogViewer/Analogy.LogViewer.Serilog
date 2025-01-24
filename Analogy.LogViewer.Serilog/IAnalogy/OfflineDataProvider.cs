571b805376744806b12ebd8f06f40d91c8f76bd6
Lior Banai <liorbanai@gmail.com>
2025-01-24 20:01:04 +0200

    Offsets from logs not working #973

--- Analogy.LogViewer.Serilog/IAnalogy/OfflineDataProvider.cs
+++ Analogy.LogViewer.Serilog/IAnalogy/OfflineDataProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Analogy.Interfaces;
+using Analogy.Interfaces.DataTypes;
 using Analogy.LogViewer.Serilog.DataTypes;
 using Analogy.LogViewer.Serilog.Managers;
 using Microsoft.Extensions.Logging;
