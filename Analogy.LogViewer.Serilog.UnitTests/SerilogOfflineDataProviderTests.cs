e09d7a1171b64569fb3f21bd22689023508dffdd
Lior Banai <liorbanai@gmail.com>
2026-02-06 22:31:23 +0200

    add more tests

---
+++ Analogy.LogViewer.Serilog.UnitTests/SerilogOfflineDataProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using Analogy.LogViewer.Serilog.IAnalogy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Analogy.LogViewer.Serilog.UnitTests
+{
+    [TestClass]
+    public class SerilogOfflineDataProviderTests
+    {
+        [TestMethod]
+        public async Task ParserFormatTest()
+        {
+            var provider = new SerilogOfflineDataProvider();
+            string fileName = Path.Combine(Environment.CurrentDirectory, "log files", "formatException.log");
+            MessageHandlerForTesting forTesting = new MessageHandlerForTesting();
+            var messages = await provider.Process(fileName, CancellationToken.None, forTesting);
+            Assert.HasCount(2, messages);
+        }
+    }
+}
